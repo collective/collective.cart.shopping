ffad01bd2566577699ce3fa96066ecf3aedb90bb
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-26 13:45:59 +0200

    Release 0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
     name='collective.cart.shopping',
-    version='0.4.0.1',
+    version='0.5',
     description="Shopping site suit for Plone.",
     long_description=long_description,
     classifiers=[
